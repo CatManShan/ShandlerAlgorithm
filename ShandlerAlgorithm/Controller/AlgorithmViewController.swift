7fc4bac5bcfe352fb69f7fd19c2d2a4ecca94315
Shan <sraw0830@740s-pl-101.local>
2018-10-23 12:29:46 -0600

    Added "Swift Image" and "Algorithm Text" and added IBOutlets to AlgorithmViewController

--- ShandlerAlgorithm/Controller/AlgorithmViewController.swift
+++ ShandlerAlgorithm/Controller/AlgorithmViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class AlgorithmViewController: UIViewController
+{
+    
+    @IBOutlet weak var algorithmText: UILabel!
+    @IBOutlet weak var swiftImage: UIImageView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
